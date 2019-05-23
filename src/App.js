ef1e70fd350eed3ea57ff2fb73835dbf5c8ee8c9
unknown <sharanya.17410@gmail.com>
2019-05-23 16:51:24 -0400

    using local storage for holding session user auth data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder'
-import {Route,Switch,withRouter} from 'react-router-dom'
+import {Route,Switch,withRouter,Redirect} from 'react-router-dom'
 import Checkout from './containers/checkout/checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
@@ -16,16 +16,30 @@ class App extends Component{
   }
 
   render(){
+    let routes =(
+      <Switch>
+        <Route path="/auth"  component={Auth}></Route>
+        <Route path="/" exact component={BurgerBuilder}></Route>
+        <Redirect to ="/"/>
+      </Switch>
+    );
+    if(this.props.isAuthenticated){
+      routes=(
+        <Switch>
+            <Route path="/checkout" component={Checkout}></Route>
+            <Route path="/orders" exact component={Orders}></Route>        
+            <Route path="/logout"  component={Logout}></Route>     
+            <Route path="/" exact component={BurgerBuilder}></Route>
+            <Redirect to ="/"/>
+        </Switch>
+      );
+    }
     return(
       <div>
         <Layout>
-          <Switch>
-              <Route path="/checkout" component={Checkout}></Route>
-              <Route path="/orders" exact component={Orders}></Route>
-              <Route path="/auth"  component={Auth}></Route>
-              <Route path="/logout"  component={Logout}></Route>
-              <Route path="/" exact component={BurgerBuilder}></Route>
-          </Switch>
+          {routes}
+              
+          
         </Layout>
       </div>
     );
@@ -33,10 +47,16 @@ class App extends Component{
   
 }
 
-const mapStateDispatchToProps=dispatch=>{
+const mapStateToProps=state=>{
+  return {
+    isAuthenticated : state.auth.token !==null
+  };
+}
+
+const mapDispatchToProps=dispatch=>{
   return{
     onTryAutoSignup :()=> dispatch(actions.authCheckState())
   };
 };
 
-export default withRouter(connect(null,mapStateDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -13,7 +13,7 @@ class Orders extends Component {
 //   }
 
   componentDidMount() {
-      this.props.onFetchOrders(this.props.token);
+      this.props.onFetchOrders(this.props.token,this.props.userId);
     //   axios.get('/orders.json')
     //       .then(res => {
     //           const fetchedOrders = [];
@@ -51,13 +51,14 @@ const mapStateToProps=state=>{
     return {
         orders:state.orders.orders,
         loading:state.orders.loading,
-        token:state.auth.token
+        token:state.auth.token,
+        userId:state.auth.userId
     }
 }
 
 const mapDispatchToProps=dispatch =>{
     return{
-        onFetchOrders:(token)=>dispatch(actions.fetchOrders(token))
+        onFetchOrders:(token,userId)=>dispatch(actions.fetchOrders(token,userId))
     }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(Orders, axios));
